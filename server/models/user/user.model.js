0b7229413afc19e6c73bc91536ca8dd51005fe90
benrotlevy <benjamin@rotlevy.com>
2022-06-29 16:00:13 +0300

    user complete and add new route of countries

--- server/models/user/user.model.js
+++ server/models/user/user.model.js
@@ -1,5 +1,53 @@
 const mongoose = require("mongoose");
-const userSchema = require("./user.schema.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    minLength: 8,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
+  uploads: [{ type: mongoose.Schema.Types.ObjectId }],
+});
+
+userSchema.methods.generateToken = async function () {
+  const token = jwt.sign({ id: this._id.toString() }, "secret string");
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  let error = false;
+  if (!user) error = true;
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) error = true;
+  if (error) throw new Error("failed to login");
+
+  return user;
+};
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
 
 const User = mongoose.model("users", userSchema);
 
--- server/routes/countries.route.js
+++ server/routes/countries.route.js
@@ -5,60 +5,80 @@ const countries = Router();
 const fakeData = [
   {
     name: "Israel",
-    tradition: {
-      description: "We love to get marry in the beach",
-      title: "fake",
-      imgURL: "",
-    },
-    food: { description: "We love Humus", title: "Humus", imgURL: "" },
-    outfit: {
-      description: "We love to dress casually",
-      title: "fake",
-      imgURL: "",
-    },
+    tradition: [
+      {
+        description: "We love to get marry in the beach",
+        title: "fake",
+        imgURL: "",
+      },
+    ],
+    food: [{ description: "We love Humus", title: "Humus", imgURL: "" }],
+    outfit: [
+      {
+        description: "We love to dress casually",
+        title: "fake",
+        imgURL: "",
+      },
+    ],
   },
   {
     name: "Canada",
-    tradition: {
-      description:
-        "At French-Canadian ceremonies, the couple's older, unmarried siblings traditionally perform a dance, all wearing wacky, brightly colored socks. As they dance, guests throw money at them which is then collected and presented to the newlyweds.",
-      title: "Money Dance",
-      imgURL:
-        "https://www.brides.com/thmb/E-VPJEnNIppBc8HrKcnm00CcLaU=/715x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/brides.com-35-335c5ad693134cb491a5203ae3bc40ce.jpg",
-    },
-    food: {
-      description:
-        "Canada’s popular poutine consists of fries and cheese curds, topped with a thick brown gravy. It was first served in the 1950s in rural snack bars in Quebec but now can be found nearly everywhere. It has become a symbol of Québécois culture.",
-      title: "Poutine",
-      imgURL:
-        "https://static.stacker.com/s3fs-public/styles/slide_desktop/s3/08poutineUMOE_0.jpg",
-    },
-    outfit: {
-      description: "We love to dress casually",
-      title: "fake",
-      imgURL: "",
-    },
+    tradition: [
+      {
+        description:
+          "At French-Canadian ceremonies, the couple's older, unmarried siblings traditionally perform a dance, all wearing wacky, brightly colored socks. As they dance, guests throw money at them which is then collected and presented to the newlyweds.",
+        title: "Money Dance",
+        imgURL:
+          "https://www.brides.com/thmb/E-VPJEnNIppBc8HrKcnm00CcLaU=/715x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/brides.com-35-335c5ad693134cb491a5203ae3bc40ce.jpg",
+      },
+    ],
+    food: [
+      {
+        description:
+          "Canada’s popular poutine consists of fries and cheese curds, topped with a thick brown gravy. It was first served in the 1950s in rural snack bars in Quebec but now can be found nearly everywhere. It has become a symbol of Québécois culture.",
+        title: "Poutine",
+        imgURL:
+          "https://static.stacker.com/s3fs-public/styles/slide_desktop/s3/08poutineUMOE_0.jpg",
+      },
+    ],
+    outfit: [
+      {
+        description: "We love to dress casually",
+        title: "fake",
+        imgURL: "",
+      },
+    ],
   },
 ];
 
 countries.get("/", (req, res) => {
   try {
-    const list = []
-    fakeData.map((country) => list.push({label: country.name, value: country.name}));
+    const list = [];
+    fakeData.map((country) =>
+      list.push({ label: country.name, value: country.name })
+    );
     res.send(list);
   } catch (e) {
     res.status(500).send({ Error: e.message });
   }
 });
 
-countries.get("/:country", (req, res) => {
+countries.get("/:country1/:country2", (req, res) => {
   try {
-    const country = fakeData.filter(
-      (country) => country.name === req.params.country
+    const countries = fakeData.filter(
+      (country) =>
+        country.name.toLowerCase() === req.params.country1.toLowerCase() ||
+        country.name.toLowerCase() === req.params.country2.toLowerCase()
     );
-    if (country.length === 0)
-      throw new Error("there is no " + req.params.country + " in dataBase");
-    res.send(country[0]);
+    if (countries.length < 2)
+      throw new Error(
+        "there is no " +
+          req.params.country1 +
+          " or " +
+          req.params.country2 +
+          " in dataBase"
+      );
+    res.send(countries);
   } catch (e) {
     res.status(500).send({ Error: e.message });
   }
