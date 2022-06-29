164892d8214c90435b31edfe89e1a0b54da2960a
ShiraOhana <soshiraohana@gmail.com>
2022-06-29 22:05:19 +0300

    css ceremony

--- client/src/pages/homepage/Home.jsx
+++ client/src/pages/homepage/Home.jsx
@@ -1,19 +1,8 @@
 import React from "react";
 import "./home.css";
-import background from "../../assets/wedding.jpg";
 
 const Home = () => {
-<<<<<<< HEAD
   return <div className="home"></div>;
-=======
-  return (
-    <div className="home">
-
-      <div style={{ backgrounImage: 'url("../../assets/wedding.jpg")' }}></div>
-
-    </div>
-  );
->>>>>>> 29ef91b1de0ac8e1139170b2fb1ef2fc3ebf1827
 };
 
 export default Home;
