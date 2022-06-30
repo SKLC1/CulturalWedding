2fb4f15b442375b5d55cafed0b3c20ba8fc8faf6
ShiraOhana <soshiraohana@gmail.com>
2022-06-30 12:32:40 +0300

    css

--- client/src/pages/UserAccount/login/login.jsx
+++ client/src/pages/UserAccount/login/login.jsx
@@ -5,7 +5,8 @@ import { authContext, useAuthContext } from "../../../context/context";
 import "./login.css";
 
 function Login() {
-  const {token, setToken, setCurrentUser, currentUser} = useAuthContext(authContext);
+  const { token, setToken, setCurrentUser, currentUser } =
+    useAuthContext(authContext);
   const [success, setSuccess] = useState(false);
   const [data, setData] = useState({
     email: "",
@@ -15,24 +16,24 @@ function Login() {
   const changeHandler = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
-  const submitHandler = async(e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      const {data} = await API.post('/users/login',{email, password});
-        setToken(data.token);
-        setCurrentUser(data.user);
-        setSuccess(true);
+      const { data } = await API.post("/users/login", { email, password });
+      setToken(data.token);
+      setCurrentUser(data.user);
+      setSuccess(true);
       console.log(data);
     } catch (error) {
       console.log(error);
     }
   };
-  if(success) {
+  if (success) {
     return (
       <div className="success">
         <h1>{`you did it - ${currentUser.email}`}</h1>
       </div>
-    )
+    );
   }
   return (
     <div className="Login_Page">
@@ -42,28 +43,33 @@ function Login() {
         </div>
         <div className="main">
           <form onSubmit={submitHandler} className="User-Info">
-            <div className="User-Email">
-              <label>Enter Email </label>
-              <br></br>
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={changeHandler}
-              />
-            </div>
+            <label className="label-login">Enter Email </label>
+
+            <br></br>
+            <input
+              className="User-Email-Lable"
+              type="email"
+              placeholder="Your email..."
+              name="email"
+              value={email}
+              onChange={changeHandler}
+            />
             <div className="User-Password">
-              <label>Enter Password </label>
+              <label className="label-login">Enter Password </label>
               <br></br>
               <input
+                className="User-Password-Lable"
                 type="password"
                 name="password"
+                placeholder="Your password..."
                 value={password}
                 onChange={changeHandler}
               />
             </div>
-            <div className="Login-Button">
-              <input type="submit" name="login" />
+            <div>
+              <button type="submit" name="login" className="Login-Button">
+                Submit
+              </button>
             </div>
           </form>
           <div className="Registration">
